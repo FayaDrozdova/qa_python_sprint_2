493018e337c0ded65d275890ffde85c4989ce5ad
FayaDrozdova <fayadrozdova@gmail.com>
2023-04-10 16:57:03 +0300

    Sprint-2 tests

--- tests.py
+++ tests.py
@@ -1,24 +1,110 @@
 from main import BooksCollector
+import pytest
 
-# класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
-# обязательно указывать префикс Test
 class TestBooksCollector:
 
-    # пример теста:
-    # обязательно указывать префикс test_
-    # дальше идет название метода, который тестируем add_new_book_
-    # затем, что тестируем add_two_books - добавление двух книг
-    def test_add_new_book_add_two_books(self):
-        # создаем экземпляр (объект) класса BooksCollector
+    @pytest.fixture(autouse=True)
+    def new_book(self):
+        self.new_book = 'Гордость и предубеждение и зомби'
+        return self.new_book
+
+    @pytest.fixture(autouse=True)
+    def another_new_book(self):
+        self.another_new_book = 'Что делать, если ваш кот хочет вас убить'
+        return self.another_new_book
+
+    @pytest.fixture(autouse=True)
+    def books_with_ratings(self):
+        self.books_with_ratings = {
+            self.new_book: 6,
+            self.another_new_book: 10,
+            '9 мест, где надо побывать с мертвой принцессой': 8
+        }
+        return self.books_with_ratings
+
+    def test_init_books_rating_type_is_dictionary(self):
         collector = BooksCollector()
 
-        # добавляем две книги
-        collector.add_new_book('Гордость и предубеждение и зомби')
-        collector.add_new_book('Что делать, если ваш кот хочет вас убить')
+        assert collector.get_books_rating() == {}
 
-        # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
+    def test_init_favorites_type_is_list(self):
+        collector = BooksCollector()
+
+        assert collector.get_list_of_favorites_books() == []
+
+    def test_add_new_book_add_two_books_count_books_two(self):
+        collector = BooksCollector()
+
+        collector.add_new_book(self.new_book)
+        collector.add_new_book(self.another_new_book)
+
         assert len(collector.get_books_rating()) == 2
 
-    # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+    def test_add_new_book_add_two_same_books_count_books_one(self):
+        collector = BooksCollector()
+
+        collector.add_new_book(self.new_book)
+        collector.add_new_book(self.new_book)
+
+        assert len(collector.get_books_rating()) == 1
+
+    def test_set_book_rating_add_rating_to_nonexistent_book_rating_is_none(self):
+        collector = BooksCollector()
+        book = self.new_book
+
+        collector.set_book_rating(book, 10)
+
+        assert collector.get_book_rating(book) is None
+
+    def test_set_book_rating_add_new_book_with_rating_less_than_one_rating_doesnt_change(self):
+        collector = BooksCollector()
+        book = self.new_book
+        rating = -1
+
+        collector.add_new_book(book)
+        collector.set_book_rating(book, rating)
+
+        assert collector.get_book_rating(book) != rating
+
+    def test_set_book_rating_add_new_book_with_rating_more_than_ten_rating_doesnt_change(self):
+        collector = BooksCollector()
+        book = self.new_book
+        rating = 15
+
+        collector.add_new_book(book)
+        collector.set_book_rating(book, rating)
+
+        assert collector.get_book_rating(book) != rating
+
+    def test_get_book_rating_get_nonexistent_book_rating_rating_is_none(self):
+        collector = BooksCollector()
+
+        assert collector.get_book_rating(self.new_book) is None
+
+    def test_add_book_in_favorites_add_new_book_book_in_favorites(self):
+        collector = BooksCollector()
+        book = self.new_book
+
+        collector.add_new_book(book)
+        collector.add_book_in_favorites(book)
+
+        assert book in collector.get_list_of_favorites_books()
+
+    def test_get_books_with_specific_rating_add_three_new_books_one_book_with_rating_more_than_eight(self):
+        collector = BooksCollector()
+
+        for new_book, book_rating in self.books_with_ratings.items():
+            collector.add_new_book(new_book)
+            collector.set_book_rating(new_book, book_rating)
+
+        assert len(collector.get_books_with_specific_rating(8)) == 1
+
+    def test_delete_book_from_favorites_add_new_book_to_favorites_count_of_books_is_zero(self):
+        collector = BooksCollector()
+        book = self.new_book
+
+        collector.add_new_book(book)
+        collector.add_book_in_favorites(book)
+        collector.delete_book_from_favorites(book)
+
+        assert len(collector.get_list_of_favorites_books()) == 0
