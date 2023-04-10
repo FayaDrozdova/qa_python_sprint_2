eed03e86244c4364ff118caeef5ba0a66905285f
FayaDrozdova <fayadrozdova@gmail.com>
2023-04-11 02:12:11 +0300

    assert message

--- tests.py
+++ tests.py
@@ -15,12 +15,12 @@ class TestBooksCollector:
     def test_init_books_rating_type_is_dictionary(self):
         collector = BooksCollector()
 
-        assert collector.get_books_rating() == {}
+        assert collector.get_books_rating() == {}, 'Books rating type is not dictionary'
 
     def test_init_favorites_type_is_list(self):
         collector = BooksCollector()
 
-        assert collector.get_list_of_favorites_books() == []
+        assert collector.get_list_of_favorites_books() == [], 'List of favorite books type is not a list'
 
     def test_add_new_book_add_two_books_count_books_two(self):
         collector = BooksCollector()
@@ -28,7 +28,7 @@ class TestBooksCollector:
         collector.add_new_book(new_book)
         collector.add_new_book(another_new_book)
 
-        assert len(collector.get_books_rating()) == 2
+        assert len(collector.get_books_rating()) == 2, 'Books arent added'
 
     def test_add_new_book_add_two_same_books_count_books_one(self):
         collector = BooksCollector()
@@ -36,7 +36,7 @@ class TestBooksCollector:
         collector.add_new_book(new_book)
         collector.add_new_book(new_book)
 
-        assert len(collector.get_books_rating()) == 1
+        assert len(collector.get_books_rating()) == 1, 'Same books added'
 
     def test_set_book_rating_add_rating_to_nonexistent_book_rating_is_none(self):
         collector = BooksCollector()
@@ -44,7 +44,7 @@ class TestBooksCollector:
 
         collector.set_book_rating(book, 10)
 
-        assert collector.get_book_rating(book) is None
+        assert collector.get_book_rating(book) is None, 'Rating of nonexistent book is set'
 
     @pytest.mark.parametrize('rating', [-1, 15])
     def test_set_book_rating_add_new_book_with_out_of_range_one_to_ten_rating_rating_not_change(self, rating):
@@ -53,12 +53,12 @@ class TestBooksCollector:
         collector.add_new_book(new_book)
         collector.set_book_rating(new_book, rating)
 
-        assert collector.get_book_rating(new_book) != rating
+        assert collector.get_book_rating(new_book) != rating, 'Rating is out of range'
 
     def test_get_book_rating_get_nonexistent_book_rating_rating_is_none(self):
         collector = BooksCollector()
 
-        assert collector.get_book_rating(new_book) is None
+        assert collector.get_book_rating(new_book) is None, 'Rating of nonexistent book is get'
 
     def test_add_book_in_favorites_add_new_book_book_in_favorites(self):
         collector = BooksCollector()
@@ -67,7 +67,7 @@ class TestBooksCollector:
         collector.add_new_book(book)
         collector.add_book_in_favorites(book)
 
-        assert book in collector.get_list_of_favorites_books()
+        assert book in collector.get_list_of_favorites_books(), 'Book isnt added to favorites'
 
     def test_get_books_with_specific_rating_add_three_new_books_one_book_with_rating_more_than_eight(self):
         collector = BooksCollector()
@@ -76,7 +76,7 @@ class TestBooksCollector:
             collector.add_new_book(book)
             collector.set_book_rating(book, book_rating)
 
-        assert len(collector.get_books_with_specific_rating(8)) == 1
+        assert len(collector.get_books_with_specific_rating(8)) == 1, 'Not one book with a rating of more than eight'
 
     def test_delete_book_from_favorites_add_new_book_to_favorites_count_of_books_is_zero(self):
         collector = BooksCollector()
@@ -86,4 +86,4 @@ class TestBooksCollector:
         collector.add_book_in_favorites(book)
         collector.delete_book_from_favorites(book)
 
-        assert len(collector.get_list_of_favorites_books()) == 0
+        assert len(collector.get_list_of_favorites_books()) == 0, 'Book isnt deleted'
