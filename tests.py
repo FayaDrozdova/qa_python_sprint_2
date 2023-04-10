8766db6b768bebd4dc8704586d9eda78c0915126
FayaDrozdova <fayadrozdova@gmail.com>
2023-04-11 02:03:24 +0300

    test data above class

--- tests.py
+++ tests.py
@@ -1,26 +1,16 @@
 from main import BooksCollector
 import pytest
 
+new_book = 'Гордость и предубеждение и зомби'
+another_new_book = 'Что делать, если ваш кот хочет вас убить'
+books_with_ratings = {
+    new_book: 6,
+    another_new_book: 10,
+    '9 мест, где надо побывать с мертвой принцессой': 8
+}
+
+
 class TestBooksCollector:
-
-    @pytest.fixture(autouse=True)
-    def new_book(self):
-        self.new_book = 'Гордость и предубеждение и зомби'
-        return self.new_book
-
-    @pytest.fixture(autouse=True)
-    def another_new_book(self):
-        self.another_new_book = 'Что делать, если ваш кот хочет вас убить'
-        return self.another_new_book
-
-    @pytest.fixture(autouse=True)
-    def books_with_ratings(self):
-        self.books_with_ratings = {
-            self.new_book: 6,
-            self.another_new_book: 10,
-            '9 мест, где надо побывать с мертвой принцессой': 8
-        }
-        return self.books_with_ratings
 
     def test_init_books_rating_type_is_dictionary(self):
         collector = BooksCollector()
@@ -35,22 +25,22 @@ class TestBooksCollector:
     def test_add_new_book_add_two_books_count_books_two(self):
         collector = BooksCollector()
 
-        collector.add_new_book(self.new_book)
-        collector.add_new_book(self.another_new_book)
+        collector.add_new_book(new_book)
+        collector.add_new_book(another_new_book)
 
         assert len(collector.get_books_rating()) == 2
 
     def test_add_new_book_add_two_same_books_count_books_one(self):
         collector = BooksCollector()
 
-        collector.add_new_book(self.new_book)
-        collector.add_new_book(self.new_book)
+        collector.add_new_book(new_book)
+        collector.add_new_book(new_book)
 
         assert len(collector.get_books_rating()) == 1
 
     def test_set_book_rating_add_rating_to_nonexistent_book_rating_is_none(self):
         collector = BooksCollector()
-        book = self.new_book
+        book = new_book
 
         collector.set_book_rating(book, 10)
 
@@ -58,7 +48,7 @@ class TestBooksCollector:
 
     def test_set_book_rating_add_new_book_with_rating_less_than_one_rating_doesnt_change(self):
         collector = BooksCollector()
-        book = self.new_book
+        book = new_book
         rating = -1
 
         collector.add_new_book(book)
@@ -68,7 +58,7 @@ class TestBooksCollector:
 
     def test_set_book_rating_add_new_book_with_rating_more_than_ten_rating_doesnt_change(self):
         collector = BooksCollector()
-        book = self.new_book
+        book = new_book
         rating = 15
 
         collector.add_new_book(book)
@@ -79,11 +69,11 @@ class TestBooksCollector:
     def test_get_book_rating_get_nonexistent_book_rating_rating_is_none(self):
         collector = BooksCollector()
 
-        assert collector.get_book_rating(self.new_book) is None
+        assert collector.get_book_rating(new_book) is None
 
     def test_add_book_in_favorites_add_new_book_book_in_favorites(self):
         collector = BooksCollector()
-        book = self.new_book
+        book = new_book
 
         collector.add_new_book(book)
         collector.add_book_in_favorites(book)
@@ -93,15 +83,15 @@ class TestBooksCollector:
     def test_get_books_with_specific_rating_add_three_new_books_one_book_with_rating_more_than_eight(self):
         collector = BooksCollector()
 
-        for new_book, book_rating in self.books_with_ratings.items():
-            collector.add_new_book(new_book)
-            collector.set_book_rating(new_book, book_rating)
+        for book, book_rating in books_with_ratings.items():
+            collector.add_new_book(book)
+            collector.set_book_rating(book, book_rating)
 
         assert len(collector.get_books_with_specific_rating(8)) == 1
 
     def test_delete_book_from_favorites_add_new_book_to_favorites_count_of_books_is_zero(self):
         collector = BooksCollector()
-        book = self.new_book
+        book = new_book
 
         collector.add_new_book(book)
         collector.add_book_in_favorites(book)
